340d3f8a0eb43b648b6c3fca24c2ebfa8fb9d1c1
Md Asaduzaman <asadasad9282@gmail.com>
2024-03-23 16:50:27 +0600

    changes

--- src/pages/Package/PackageList.tsx
+++ src/pages/Package/PackageList.tsx
@@ -134,7 +134,7 @@ const PackageList = () => {
                         {packageItem.status}
                       </p>
                     </td> */}
-                  <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                  <td className="border-b border-[#eee] py-5 px-3 dark:border-strokedark">
                     <div className="flex items-center space-x-3.5">
                       <button className="hover:text-primary">
                         <svg
@@ -183,7 +183,9 @@ const PackageList = () => {
                         </svg>
                       </button>
                       <button className="hover:text-primary">
-
+                        <svg className="w-6 h-6 text-gray-800  " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z" />
+                        </svg>
                       </button>
 
                     </div>
