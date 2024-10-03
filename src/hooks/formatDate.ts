ddd8386fb187a792640339cd0660c6b084dbca04
Asaduzzaman14 <asadasad9282@gmail.com>
2024-10-03 12:03:08 +0600

    date fromate change

--- src/hooks/formatDate.ts
+++ src/hooks/formatDate.ts
@@ -1,4 +1,13 @@
+// export const formatToLocalDate = (date: string) => {
+//   const localDate = new Date(date).toLocaleString();
+//   return localDate;
+// };
+
 export const formatToLocalDate = (date: string) => {
-  const localDate = new Date(date).toLocaleString();
+  const localDate = new Date(date).toLocaleDateString('en-GB', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+  });
   return localDate;
 };
